050791c4d7b0d5c1f6238f8c632ab2e41f6310b7
bradleymont <bradleymont@gmail.com>
2020-05-03 00:08:42 -0700

    implemented maximal nonbranching path algorithm - only 4% coverage :(

--- project3/basic_assembly.py
+++ project3/basic_assembly.py
@@ -38,9 +38,134 @@ def parse_reads_file(reads_fn):
 
 """
     TODO: Use this space to implement any additional functions you might need
-
 """
 
+# returns an adjacency list representation of the de Bruijn graph
+def form_de_bruijn_graph(kmers):
+    # for each k-mer:
+    #   prefix -> suffix
+    adjacency_list = {}
+    for kmer in kmers:
+        prefix = kmer[:-1]
+        suffix = kmer[1:]
+        if prefix in adjacency_list:
+            adjacency_list[prefix].append(suffix)
+        else:
+            adjacency_list[prefix] = [suffix]
+
+    return adjacency_list
+
+# returns the degrees for each node in a graph
+# node -> [in-degree,out-degree]
+def get_node_degrees(adjacency_list):
+    node_degrees = {}
+    for outgoing_node, incoming_nodes in adjacency_list.items():
+        # increment the out-degree for outgoing_node by the number of incoming nodes
+        if outgoing_node in node_degrees:
+            node_degrees[outgoing_node][1] += len(incoming_nodes)
+        else:
+            node_degrees[outgoing_node] = [0, len(incoming_nodes)]
+
+        # increment the in-degree for each incoming node by 1
+        for incoming_node in incoming_nodes:
+            if incoming_node in node_degrees:
+                node_degrees[incoming_node][0] += 1
+            else:
+                node_degrees[incoming_node] = [1, 0]
+
+    return node_degrees
+
+# returns all maximal non-branching paths in De Bruijn Graph
+def find_paths(adjacency_list):
+
+    # find in-degrees and out-degrees for each node
+    node_degrees = get_node_degrees(adjacency_list)
+
+    # find maximal non-branching paths
+
+    # Paths ← empty list
+    paths = []
+
+    # for each node v in graph
+    for v in list(node_degrees.keys()):
+        # if v is not a 1-in-1-out node
+        if node_degrees[v] != [1,1]:
+            # if out(v) > 0
+            if node_degrees[v][1] > 0:
+                # for each outgoing edge (v, w) from v
+                for w in adjacency_list[v]:
+                    # NonBranchingPath ← the path consisting of single edge (v, w)
+                    non_branching_path = [v,w]
+                    # while w is a 1-in-1-out node
+                    while node_degrees[w] == [1,1]:
+                        # extend NonBranchingPath by the edge (w, u)
+                        u = adjacency_list[w][0]
+                        non_branching_path.append(u)
+                        # w ← u
+                        w = u
+                    # add NonBranchingPath to the set Paths
+                    paths.append(non_branching_path)
+
+    # for each isolated cycle Cycle in Graph
+    #   add Cycle to Paths
+
+    # isolated cycles won't contain any of the nodes traversed in paths, so remove those nodes from our graph
+    for path in paths:
+        for node in path:
+            if node in adjacency_list:
+                del adjacency_list[node]
+
+    # remove any nodes that aren't 1-in-1-out nodes
+    for node in list(adjacency_list.keys()):
+        # if node isn't a 1-in-1-out node
+        if node_degrees[node] != [1,1]:
+            del adjacency_list[node]
+
+    # adjacency_list now consists of only 1-in-1-out nodes that weren't traversed in any of our other paths
+    # while adjacency_list is not empty
+    while adjacency_list:
+        # select any node as the starting node for our cycle
+        start_node = list(adjacency_list.keys())[0]
+        
+        curr_node = start_node
+        next_node = adjacency_list[start_node][0]
+
+        cycle = [start_node]
+
+        first_time_visiting_start_node = True
+        while curr_node != start_node or first_time_visiting_start_node:
+            first_time_visiting_start_node = False
+
+            # remove curr_node from adjacency_list
+            del adjacency_list[curr_node]
+
+            # add next_node to cycle
+            cycle.append(next_node)
+
+            # update curr_node and next_node
+            curr_node = next_node
+
+            # make sure we haven't reached a dead end
+            if next_node not in adjacency_list:
+                continue
+            next_node = adjacency_list[next_node][0]
+        
+        # add our cycle to paths
+        paths.append(cycle)
+
+    return paths
+
+# returns the contigs formed by the maximal non-branching paths
+def form_contigs(paths):
+    contigs = []
+
+    for path in paths:
+        contig = path[0] # add the entire first kmer to the contig
+        for i in range(1, len(path)):
+            contig += path[i][-1] # add the end of each kmer to the contig
+        contigs.append(contig)
+
+    return contigs
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='basic_assembly.py takes in data for homework assignment 3 consisting '
@@ -66,11 +191,26 @@ if __name__ == "__main__":
     """
     TODO: Call functions to do the actual assembly here
     """
-    print(input_reads)
+
+    # TODO: WE CAN ALSO JUST MODIFY PARSE_READ_FILES BUT IM NOT SURE IF WE CAN CHANGE IT
+    # Note: in order to avoid issues with variable length read pairs, we will
+    # consider each part of the read pair as its own independent read
+    # therefore, we "flatten" the input_reads list so each read is its own entry
+    input_reads = [read for read_pair in input_reads for read in read_pair]
+
+    ###### STEP 1: CONSTRUCT DE BRUIJN GRAPH ######
+    # adjacency list representation of the de Bruijn graph
+    adjacency_list = form_de_bruijn_graph(input_reads)
+
+    ###### STEP 2: FIND ALL MAXIMAL NON-BRANCHING PATHS IN DE BRUIJN GRAPH ######
+    paths = find_paths(adjacency_list)
+
+    ###### STEP 3: FORM CONTIGS FROM MAXIMAL NON-BRANCHING PATHS ######
+    contigs = form_contigs(paths)
 
     # replace contigs with a list of the contigs that I recover
     #contigs = ['GCTGACTAGCTAGCTACGATCGATCGATCGATCGATCGATGACTAGCTAGCTAGCGCTGACT']
-    contigs = ["contig1","contig2","contig3"]
+    #contigs = ["contig1","contig2","contig3"]
 
     output_fn = args.output_file
     zip_fn = output_fn + '.zip'
